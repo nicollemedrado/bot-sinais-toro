64d6db7f6f436359d2f4d54d12c379d1013573b4
nicollemedrado <atenasnicolle15@gmail.com>
2025-05-18 00:00:20 -0300

    Delete teste_telegram.py

--- teste_telegram.py
+++
@@ -1,8 +0,0 @@
-from telegram import Bot
-
-# Token e ID fixos (apenas para teste rápido)
-TOKEN = "7682503896:AAH8AniMCJHIs8s-mfeE23TWqArmiXto_FU"
-CHAT_ID = "5845175811"
-
-bot = Bot(token=TOKEN)
-bot.send_message(chat_id=CHAT_ID, text="🚀 Teste de envio de mensagem com sucesso!")
